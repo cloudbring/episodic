ccfc217cc566502b9ebf0128fa529d8a8d07f29e
Joshua Munene <joshuamwangi@gmail.com>
2015-02-17 14:19:47 -0800

    add create rewrite migration to add tables to the schema.rb

--- db/migrate/20150217221334_create_rewrites.rb
+++ db/migrate/20150217221334_create_rewrites.rb
@@ -1,7 +1,6 @@
 class CreateRewrites < ActiveRecord::Migration
   def change
     create_table :rewrites do |t|
-      t.integer :id
       t.string :body
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217215924) do
+ActiveRecord::Schema.define(version: 20150217221334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rewrites", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tvshows", force: :cascade do |t|
     t.datetime "created_at", null: false
