caad3d6595a6bee174819b90da6959c551532c5c
Joshua Munene <joshuamwangi@gmail.com>
2015-02-17 21:41:36 -0800

    Adding factories and tvshow model tests

--- spec/models/tvshow_spec.rb
+++ spec/models/tvshow_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Tvshow, type: :model do
   context "with the show The Good Wife" do
-    tvshow = Tvshow.create!(
-      :title => "the good wife", 
-      :trakt_id => 1426, 
-      :poster_image => "https://walter.trakt.us/images/shows/000/001/426/posters/medium/749eeff986.jpg")
+    before(:each) do
+      @tvshow = FactoryGirl.create(:the_good_wife)
+    end
+
     it "has a title" do
-      expect(tvshow.title).to eq("the good wife")
+      expect(@tvshow.title).to eq("The Good Wife")
     end
     it "has a trakt_id" do
-      expect(tvshow.trakt_id).to eq(1426)
+      expect(@tvshow.trakt_id).to eq(1426)
     end
     it "has a poster_image" do
-      expect(tvshow.poster_image).to eq("https://walter.trakt.us/images/shows/000/001/426/posters/medium/749eeff986.jpg")
+      expect(@tvshow.poster_image).to eq("https://walter.trakt.us/images/shows/000/001/426/posters/medium/749eeff986.jpg")
     end
   end
 end
